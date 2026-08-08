6647a5bd9835adbab0c12ccd9bb085d99da069e0
shekharnalanda <shekharnalanda@gmail.com>
2026-08-08 12:06:17 +0530

    feat: add school settings model

---
+++ app/Models/SchoolSetting.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SchoolSetting extends Model
+{
+    protected $fillable = [
+        'school_name',
+        'logo',
+        'phone',
+        'email',
+        'address',
+        'website',
+    ];
+}
