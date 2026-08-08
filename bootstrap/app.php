9816caa48921ed48d5cdc544ba1fd02a68464a58
shekharnalanda <shekharnalanda@gmail.com>
2026-08-08 12:43:34 +0530

    fix: configure Laravel exception handling bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -14,5 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        // Application-level exception handling configuration.
     })
     ->create();
