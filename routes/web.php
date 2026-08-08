3d129c2d03f293957296831999919ee7e4b79034
shekharnalanda <shekharnalanda@gmail.com>
2026-08-08 12:09:21 +0530

    feat: protect admin routes with authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\GalleryController;
 use App\Http\Controllers\Admin\NoticeController;
 use App\Http\Controllers\Admin\SchoolSettingController;
@@ -12,16 +13,24 @@ Route::view('/gallery', 'gallery')->name('gallery');
 Route::view('/contact', 'contact')->name('contact');
 
 Route::prefix('admin')->name('admin.')->group(function () {
-    Route::view('/', 'admin.dashboard')->name('dashboard');
-    Route::get('/notices', [NoticeController::class, 'index'])->name('notices.index');
-    Route::post('/notices', [NoticeController::class, 'store'])->name('notices.store');
-    Route::put('/notices/{notice}', [NoticeController::class, 'update'])->name('notices.update');
-    Route::delete('/notices/{notice}', [NoticeController::class, 'destroy'])->name('notices.destroy');
-    Route::get('/gallery', [GalleryController::class, 'index'])->name('gallery.index');
-    Route::post('/gallery', [GalleryController::class, 'store'])->name('gallery.store');
-    Route::delete('/gallery/{gallery}', [GalleryController::class, 'destroy'])->name('gallery.destroy');
-    Route::get('/settings', [SchoolSettingController::class, 'edit'])->name('settings.edit');
-    Route::put('/settings', [SchoolSettingController::class, 'update'])->name('settings.update');
+    Route::middleware('guest')->group(function () {
+        Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+        Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/', function () { return view('admin.dashboard'); })->name('dashboard');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+        Route::get('/notices', [NoticeController::class, 'index'])->name('notices.index');
+        Route::post('/notices', [NoticeController::class, 'store'])->name('notices.store');
+        Route::put('/notices/{notice}', [NoticeController::class, 'update'])->name('notices.update');
+        Route::delete('/notices/{notice}', [NoticeController::class, 'destroy'])->name('notices.destroy');
+        Route::get('/gallery', [GalleryController::class, 'index'])->name('gallery.index');
+        Route::post('/gallery', [GalleryController::class, 'store'])->name('gallery.store');
+        Route::delete('/gallery/{gallery}', [GalleryController::class, 'destroy'])->name('gallery.destroy');
+        Route::get('/settings', [SchoolSettingController::class, 'edit'])->name('settings.edit');
+        Route::put('/settings', [SchoolSettingController::class, 'update'])->name('settings.update');
+    });
 });
 
-Route::get('/login', function () { return response('Login module coming next.', 200); })->name('login');
+Route::get('/login', fn () => redirect()->route('admin.login'))->name('login');
