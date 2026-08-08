41bcbd5bfda2e4ee4856dbfd01ddf06c8d8bea9b
shekharnalanda <shekharnalanda@gmail.com>
2026-08-08 12:45:51 +0530

    fix: add application exception handler

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * The inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+}
