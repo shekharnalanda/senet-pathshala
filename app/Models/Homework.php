9d10d181350af7c20a4eac434c4a08f79c548d98
shekharnalanda <shekharnalanda@gmail.com>
2026-08-17 13:32:59 +0530

    Add homework model

---
+++ app/Models/Homework.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Homework extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['class_name', 'section', 'subject', 'homework_date', 'due_date', 'details'];
+
+    protected $casts = [
+        'homework_date' => 'date',
+        'due_date' => 'date',
+    ];
+}
