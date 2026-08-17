3fd36402beb67226c3d514617e505d798836381a
shekharnalanda <shekharnalanda@gmail.com>
2026-08-17 12:12:30 +0530

    Add admin student details route

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('/students', [StudentController::class, 'index'])->name('students.index');
         Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
         Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+        Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
         Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
         Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
         Route::get('/student-id-cards', [StudentIdCardController::class, 'index'])->name('student-id-cards.index');
