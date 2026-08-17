8b64419fe8a6a5606b856ceb9545406ea48a5694
shekharnalanda <shekharnalanda@gmail.com>
2026-08-17 10:45:39 +0530

    Add admin student management routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\GalleryController;
 use App\Http\Controllers\Admin\NoticeController;
 use App\Http\Controllers\Admin\SchoolSettingController;
+use App\Http\Controllers\Admin\StudentController;
 use App\Http\Controllers\Admin\StudentIdCardController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
@@ -25,7 +26,6 @@ Route::prefix('student')->name('student.')->group(function () {
         Route::get('/login', [StudentAuthController::class, 'showLogin'])->name('login');
         Route::post('/login', [StudentAuthController::class, 'login'])->name('login.submit');
     });
-
     Route::middleware('auth')->group(function () {
         Route::get('/', [StudentDashboardController::class, 'index'])->name('dashboard');
         Route::get('/profile', [StudentDashboardController::class, 'profile'])->name('profile');
@@ -39,10 +39,16 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
         Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
     });
-
     Route::middleware(['auth', 'admin'])->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+        Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+        Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+        Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+        Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
+        Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
+        Route::get('/student-id-cards', [StudentIdCardController::class, 'index'])->name('student-id-cards.index');
+        Route::post('/student-id-cards/print', [StudentIdCardController::class, 'print'])->name('student-id-cards.print');
         Route::get('/notices', [NoticeController::class, 'index'])->name('notices.index');
         Route::post('/notices', [NoticeController::class, 'store'])->name('notices.store');
         Route::put('/notices/{notice}', [NoticeController::class, 'update'])->name('notices.update');
@@ -54,9 +60,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::put('/settings', [SchoolSettingController::class, 'update'])->name('settings.update');
         Route::get('/contacts', [AdminContactController::class, 'index'])->name('contacts.index');
         Route::delete('/contacts/{contact}', [AdminContactController::class, 'destroy'])->name('contacts.destroy');
-        Route::get('/student-id-cards', [StudentIdCardController::class, 'index'])->name('student-id-cards.index');
-        Route::post('/student-id-cards/print', [StudentIdCardController::class, 'print'])->name('student-id-cards.print');
     });
 });
-
 Route::get('/login', fn () => redirect()->route('admin.login'))->name('login');
