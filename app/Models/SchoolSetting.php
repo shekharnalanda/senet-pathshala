aaab40fb6763f423bae52fbbc3dc9da6242e8890
shekharnalanda <shekharnalanda@gmail.com>
2026-08-17 18:38:53 +0530

    Add website CMS fields to school settings

--- app/Models/SchoolSetting.php
+++ app/Models/SchoolSetting.php
@@ -7,12 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class SchoolSetting extends Model
 {
     protected $fillable = [
-        'school_name',
-        'logo',
-        'phone',
-        'email',
-        'address',
-        'website',
-        'academic_session',
+        'school_name','logo','phone','email','address','website','academic_session',
+        'admission_text','footer_about','main_campus','branch_office','footer_bottom_text',
+        'home_heading','home_subheading','about_heading','about_content','gallery_heading','gallery_subheading',
     ];
 }
