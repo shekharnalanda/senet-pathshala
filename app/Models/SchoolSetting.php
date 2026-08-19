2c51f2bd977dbee8b541d4dd96cbe14fa6887988
shekharnalanda <shekharnalanda@gmail.com>
2026-08-19 12:05:22 +0530

    Add admission page image settings

--- app/Models/SchoolSetting.php
+++ app/Models/SchoolSetting.php
@@ -10,6 +10,8 @@ class SchoolSetting extends Model
         'school_name','logo','phone','email','address','website','academic_session',
         'admission_text','footer_about','main_campus','branch_office','footer_bottom_text',
         'home_heading','home_subheading','home_hero_image','about_heading','about_content',
-        'about_hero_image','about_image_one','about_image_two','gallery_heading','gallery_subheading',
+        'about_hero_image','about_image_one','about_image_two',
+        'admission_hero_image','admission_image_one','admission_image_two',
+        'gallery_heading','gallery_subheading',
     ];
 }
