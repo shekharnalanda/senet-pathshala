f94ea1e55e208f7e8ea84fa53836ef21f19adbb9
shekharnalanda <shekharnalanda@gmail.com>
2026-08-21 13:55:07 +0530

    Link admission applications to campuses

--- app/Models/AdmissionApplication.php
+++ app/Models/AdmissionApplication.php
@@ -3,6 +3,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class AdmissionApplication extends Model
 {
-    protected $fillable=['application_no','student_name','date_of_birth','gender','class_applied','father_name','mother_name','guardian_name','phone','alternate_phone','email','address','previous_school','message','status','admin_note','reviewed_at'];
+    protected $fillable=['branch_id','application_no','student_name','date_of_birth','gender','class_applied','father_name','mother_name','guardian_name','phone','alternate_phone','email','address','previous_school','message','status','admin_note','reviewed_at'];
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
     protected $casts=['date_of_birth'=>'date','reviewed_at'=>'datetime'];
 }
