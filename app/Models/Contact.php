d310761306582df5ab31eb52e50b9450649adce7
shekharnalanda <shekharnalanda@gmail.com>
2026-08-21 15:44:17 +0530

    Link enquiries to campuses

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,13 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contact extends Model
 {
     protected $fillable = [
+        'branch_id',
         'name',
         'email',
         'mobile',
         'message',
     ];
+
+    public function branch(): BelongsTo
+    {
+        return $this->belongsTo(Branch::class);
+    }
 }
